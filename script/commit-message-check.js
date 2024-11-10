38db2b1a949a736480393ae20151aebdf54858d4
Jay <Jay040030@outlook.com>
2024-11-10 22:31:01 +0800

    chore: 更新Husky钩子

--- script/commit-message-check.js
+++ script/commit-message-check.js
@@ -1,3 +1,4 @@
+// git 的 commit-msg 钩子脚本，用于检查 commit message 格式
 // 读取 .git/COMMIT_EDITMSG 文件内容（即，commit message）
 const msgPath = process.argv[2]
 const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
@@ -7,7 +8,7 @@ const commitRE = /^(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|cho
 if (!commitRE.test(msg)) {
   console.error(`
         不合法的 commit 消息格式。
-        请查看 git commit 提交规范：
+        请查看 git commit 提交规范：https://z-j-wang.github.io/front-end-engineering/git%E7%AF%87-commit%E8%A7%84%E8%8C%83.html
     `)
 
   process.exit(1)
