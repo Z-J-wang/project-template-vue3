78c6e4bbdd9ff0fad756c3b974bcb30dc6478366
Jay <Jay040030@outlook.com>
2025-07-03 13:51:50 +0800

    chore(stylelint): 升级 stylelint 及相关配置
    - 将 stylelint 从 v13.3.3 升级到 v16.21.0
    - 更新 .stylelintignore 文件，忽略更多文件类型
    - 修改 .stylelintrc.js 配置，增加对 Less 和 Vue 的支持
    - 更新 package.json 中的 lint 脚本
    - 修改多个 Vue 组件和 CSS 文件中的媒体查询方式

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,44 +1,20 @@
-/**
- * stylelint 依赖如下：
- * "stylelint": "^13.3.3"
- * "stylelint-config-standard": "^20.0.0"
- * "stylelint-order": "^6.0.3"
- *
- * vue2 中 stylelint 需要v13及以下版本
- * stylelint-config-standard v20.0.0为stylelint v13.3.3 配套版本
- * stylelint-order 用于样式属性排序
- */
-
+/** @type {import('stylelint').Config} */
 module.exports = {
-  extends: ['stylelint-config-standard'],
+  root: true,
+  extends: [
+    'stylelint-config-standard', // 配置 stylelint 拓展插件
+    'stylelint-config-standard-less',
+    'stylelint-config-html/vue',
+  ],
   plugins: ['stylelint-order'],
   rules: {
-    'color-hex-case': 'lower', // 颜色指定小写
-    'block-no-empty': true, // 禁止空块
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind']
+      }
+    ],
     'color-hex-length': 'long', // 颜色6位长度
-    'declaration-block-trailing-semicolon': ['always', { ignore: ['single-declaration'] }], // 在声明块中使用尾随分号
-    // 兼容自定义标签名
-    'selector-type-no-unknown': [
-      true,
-      {
-        ignoreTypes: []
-      }
-    ],
-    // 忽略伪类选择器 ::v-deep
-    'selector-pseudo-element-no-unknown': [
-      true,
-      {
-        ignorePseudoElements: ['v-deep']
-      }
-    ],
-    'no-descending-specificity': null, // 禁止低优先级的选择器出现在高优先级的选择器之后。
-    'at-rule-no-unknown': null, // 不验证@未知的名字，为了兼容scss的函数
-    'comment-no-empty': true, // 禁止空注释
-    'shorthand-property-no-redundant-values': true, // 禁止简写属性的冗余值
-    'value-no-vendor-prefix': true, // 禁止值的浏览器引擎前缀
-    'property-no-vendor-prefix': true, // property-no-vendor-prefix
-    'number-leading-zero': 'always', // 禁止小于 1 的小数有一个前导零
-    'no-empty-first-line': true, // 禁止空第一行
     // 属性的排序
     'order/properties-order': [
       'position',
@@ -116,7 +92,7 @@ module.exports = {
       'box-shadow',
       'text-shadow',
       'resize',
-      'transition'
-    ]
-  }
-};
+      'transition',
+    ],
+  },
+}
--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -40,7 +40,7 @@ h3 {
   color: var(--color-heading);
 }
 
-@media (min-width: 1024px) {
+@media (width >= 1024px) {
   .item {
     margin-top: 0;
     padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
