bbf3eab75af45522d23ed543a3e8ea7752ccd2ff
Jay <42605263+Z-J-wang@users.noreply.github.com>
2025-07-15 16:43:09 +0800

    chore: 取消TailwindCSS基础样式的引入，并补充说明 (#13)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false // 禁用全局样式重置，以避免样式冲突。关闭此选项会取消基础样式的引入。本项目引入 TailwindCSS 只是为了使用工具类，故关闭。
+  },
   prefix: '',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], // tailwind CSS 生效路劲
   theme: {
